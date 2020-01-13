c39d5c096a733437225ab314d7919d71dc490f90
Yug Thapar <yugthapar37@gmail.com>
2020-01-13 18:16:48 +0530

    Completed Cancer & WrittenDigit Determiner Project

--- Projects/CancerDeterminer/CancerDeterminer.py
+++ Projects/CancerDeterminer/CancerDeterminer.py
@@ -1,6 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from matplotlib.colors import ListedColormap
 from sklearn import datasets
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 
-cancer = datasets.load_breast_cancer()
+dataset = datasets.load_breast_cancer()
 
-print("Features: ", cancer.feature_names)
-print("Labels: ", cancer.target_names)
+X = dataset.data
+y = dataset.target
+
+Xtrain, Xtest, yTrain, yTest = train_test_split(
+    X, y, test_size=0.2, random_state=0)
+
+scX = StandardScaler()
+Xtrain = scX.fit_transform(Xtrain)
+Xtest = scX.transform(Xtest)
+
+classifier = SVC(kernel="rbf", random_state=0)
+classifier.fit(Xtrain, yTrain)
+
+yPred = classifier.predict(Xtest)
+
+cm = confusion_matrix(yTest, yPred)
+
+accuracy = accuracy_score(yTest, yPred)
+precision = int((cm[1][1] / (cm[1][1] + cm[0][1])) * 10**3) / 10**3
+recall = int((cm[1][1] / (cm[1][1] + cm[1][0])) * 10**3) / 10**3
+f1Score = int((2 * precision * recall / (precision + recall)) * 10**3) / 10**3
--- Projects/WrittenDigitDeterminer/WrittenDigitDeterminer.py
+++ Projects/WrittenDigitDeterminer/WrittenDigitDeterminer.py
@@ -1,8 +1,31 @@
-from sklearn.preprocessing import scale
-from sklearn.datasets import load_digits
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import normalize
 
-digits = load_digits()
-data = scale(digits.data)
-y = digits.target
+(Xtrain, yTrain), (Xtest, yTest) = mnist.load_data()
 
-samples, features = data.shape
+Xtrain = normalize(Xtrain, axis=1)
+Xtest = normalize(Xtest, axis=1)
+
+classifier = Sequential()
+classifier.add(Flatten())
+classifier.add(Dense(128, activation=tf.nn.relu))
+classifier.add(Dense(128, activation=tf.nn.relu))
+classifier.add(Dense(10, activation=tf.nn.softmax))
+classifier.compile(optimizer="adam",
+                   loss="sparse_categorical_crossentropy",
+                   metrics=["accuracy"])
+
+classifier.fit(
+    Xtrain,
+    yTrain,
+    epochs=100,
+)
+
+valLoss, valAccu = classifier.evaluate(Xtest, yTest)
+
+yPred = np.argmax(classifier.predict([Xtest])[0])
